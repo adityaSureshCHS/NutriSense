8adc3f503d58625e1e82b373498d3bb7beec6022
Aditya Suresh <asuresh952@student.fuhsd.org>
2024-05-25 15:00:58 -0700

    sdsd
    dsdsds

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ import cv2 as cv
 from flask import Flask, request, render_template, url_for, redirect
 from openai import OpenAI
 import pytesseract
+import google.generativeai as genai
+import PIL.Image
+from urllib import request as req
+import io
+import base64
 
-
+gemini_key = "AIzaSyCvHRoH7oHsJa8J2D4kZIOJ5qxG4ZNHXm0"
+genai.configure(api_key = gemini_key)
 app = Flask(__name__)
 
 
@@ -23,5 +29,10 @@ def results():
     if request.method == 'POST':
         global data
         data = request.form['image']
+        data = req.urlopen(data).read()
+        img = PIL.Image.open(io.BytesIO(data))
+        model = genai.GenerativeModel('gemini-pro-vision')
+        response = model.generate_content(["Identify the ingredients. List each ingredient. Do not add extra characters, dashes, parenthesis, or commas. List the ingredients", img])
+        print(response.text)
         
     return render_template("results.html")
