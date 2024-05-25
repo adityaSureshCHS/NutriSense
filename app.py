9cf674ad840766cf0ba70b30ca3fa19885bffdc5
KGupt10 <imkavyagupta@gmail.com>
2024-05-25 11:54:32 -0700

    changed style.css

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 import os 
+from flask import Flask, request, render_template, redirect, url_for
 import cv2 as cv
-from flask import Flask, request, render_template
+import pytesseract
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "hello world"
+
+@app.route("/")
+def main():
+    return render_template("main.html", path = url_for("photo"))
 
 
+@app.route("/photo")
+def photo():
+    return render_template("photo.html", path = url_for("results"))
+    
+@app.route("/results", methods=["POST", "GET"])
+def results():
+    if request.method == 'POST':
+        data = request.form['image']
+        print(data)
+    return render_template("results.html")
 
